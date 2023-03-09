bf7113339c61fd8de9bf798d4f59b46ce0f5947f
phandyph <phornphandy23@gmail.com>
2023-03-09 15:23:28 +0700

    #4 function and some info for displaypagination component

---
+++ pagination/src/component/displaypagination/Displaypagination.jsx
@@ -0,0 +1,9 @@
+import './Displaypagination.css';
+
+const Displaypagination = () => {
+    return (
+        <div>Pagination</div>
+    )
+}
+
+export default Displaypagination;
--- pagination/src/component/parentcompo/Parent.jsx
+++ pagination/src/component/parentcompo/Parent.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
+import Displaypagination from "../displaypagination/Displaypagination";
 import './Parent.css';
 const URL = 'https://jsonplaceholder.typicode.com/users';
 
 const Parent = () => {
     const [loading, setLoading] = useState(true);
     const [users, setUsers] = useState([])
+    const fivePages = Array.from(Array(users.length/2).keys())
+    const [currentPage, setCurrentPage] = useState('');
+    const [twoUsers, setTwoUsers] = useState([]);
+
+
     /**
      * Fetch data from URL of users (contain 10 objects.)
      */
@@ -20,8 +26,35 @@ const Parent = () => {
     }, [])
 
 
+    const onClickCurrentPage = (para) => {
+        setCurrentPage(para)
+        setTwoUsers([users[para*2], users[para*2+1]])
+
+    }
+
+    const onClickPreviousPage = (para) => {
+        setCurrentPage(para)
+        setTwoUsers([users[para*2], users[para*2+1]])
+
+    }
+
+    const onClickNextPage = (para) => {
+        setCurrentPage(para)
+        setTwoUsers([users[para*2], users[para*2+1]])
+    }
+
     return (
-        <div>Parent Component</div>
+        <div className="new-paginate">
+            <Displaypagination 
+                fivePages={fivePages}
+                onClickCurrentPage={onClickCurrentPage}
+                onClickPreviousPage={onClickPreviousPage}
+                onClickNextPage={onClickNextPage}
+                users={users}
+                twoUsers={twoUsers}
+                currentPage={currentPage}
+            />
+        </div>
     )
 }
 
